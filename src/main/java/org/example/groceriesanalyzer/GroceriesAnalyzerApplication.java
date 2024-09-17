8873eb66f5fbb30049d3f2e209c3fc918e094c2b
alexandre <zatti.alexandre61@gmail.com>
2024-09-16 22:05:40 -0300

    initial commit

---
+++ src/main/java/org/example/groceriesanalyzer/GroceriesAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package org.example.groceriesanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceriesAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceriesAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/groceriesanalyzer/GroceriesAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.groceriesanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceriesAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
