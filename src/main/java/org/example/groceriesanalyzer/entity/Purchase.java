a962ca875df9b7f673a6988ae11463f066bf4797
alexandre <zatti.alexandre61@gmail.com>
2024-11-09 19:01:54 -0300

    Fixing date problems

--- src/main/java/org/example/groceriesanalyzer/entity/Purchase.java
+++ src/main/java/org/example/groceriesanalyzer/entity/Purchase.java
@@ -2,7 +2,10 @@ package org.example.groceriesanalyzer.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -10,7 +13,8 @@ import java.util.List;
 @Document(indexName = "purchases")
 public record Purchase(
         @Id String id,
-        @JsonFormat(pattern = "yyyy-MM-dd")  // Ensures LocalDate is serialized correctly
+        @Field(type = FieldType.Date, format = DateFormat.date)
+        @JsonFormat(pattern = "yyyy-MM-dd", timezone = "UTC")
         LocalDate date,
         String code,
         String description,
